5d9fe36bc7ca35981cc679ca883de454c8bb1860
Beavya <AnnBezymya@gmail.com>
2025-11-12 21:50:12 +0700

    add main page

--- main/views.py
+++ main/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Application
 
-# Create your views here.
+def index(request):
+    applications_in_progress_count = Application.objects.filter(status='in_progress').count()
+
+    completed_applications = Application.objects.filter(status='completed').order_by('-created_at')[:4]
+
+    context = {
+        'applications_in_progress_count': applications_in_progress_count,
+        'completed_applications': completed_applications,
+    }
+    return render(request, 'index.html', context)
