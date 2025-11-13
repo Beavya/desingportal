61b596f54edf39126c7811b2cac57c356cfbf2c9
Beavya <AnnBezymya@gmail.com>
2025-11-13 12:41:16 +0700

    add register, login, logout

---
+++ main/forms.py
@@ -0,0 +1,64 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+import re
+from django.contrib.auth.forms import AuthenticationForm
+
+class UserRegisterForm(UserCreationForm):
+    last_name = forms.CharField(max_length=100, label='Фамилия')
+    first_name = forms.CharField(max_length=100, label='Имя')
+    patronymic = forms.CharField(max_length=100, label='Отчество', required=False)
+    agree = forms.BooleanField(label='Согласие на обработку персональных данных')
+
+    class Meta:
+        model = User
+        fields = ['last_name', 'first_name', 'patronymic', 'username', 'email', 'password1', 'password2', 'agree']
+
+    def clean_last_name(self):
+        last_name = self.cleaned_data.get('last_name')
+        if not re.match(r'^[а-яА-ЯёЁ\s\-]+$', last_name):
+            raise forms.ValidationError("Фамилия должна содержать только буквы кириллицы, дефис и пробелы")
+        return last_name
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+        if not re.match(r'^[а-яА-ЯёЁ\s\-]+$', first_name):
+            raise forms.ValidationError("Имя должно содержать только буквы кириллицы, дефис и пробелы")
+        return first_name
+
+    def clean_patronymic(self):
+        patronymic = self.cleaned_data.get('patronymic')
+        if patronymic and not re.match(r'^[а-яА-ЯёЁ\s\-]+$', patronymic):
+            raise forms.ValidationError("Отчество должно содержать только буквы кириллицы, дефис и пробелы")
+        return patronymic
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if not re.match(r'^[a-zA-Z\-]+$', username):
+            raise forms.ValidationError("Логин должен содержать только латиницу и дефис")
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError("Этот логин уже занят")
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Этот email уже используется")
+        return email
+
+    def clean_agree(self):
+        agree = self.cleaned_data.get('agree')
+        if not agree:
+            raise forms.ValidationError("Необходимо согласие на обработку персональных данных")
+        return agree
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.last_name = self.cleaned_data['last_name']
+        user.first_name = self.cleaned_data['first_name']
+        if commit:
+            user.save()
+        return user
+
+class UserLoginForm(AuthenticationForm):
+    pass
--- main/views.py
+++ main/views.py
@@ -1,13 +1,39 @@
-from django.shortcuts import render
 from .models import Application
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from .forms import UserRegisterForm
+from .forms import UserLoginForm
+
 
 def index(request):
-    applications_in_progress_count = Application.objects.filter(status='in_progress').count()
+    return render(request, 'index.html')
 
-    completed_applications = Application.objects.filter(status='completed').order_by('-created_at')[:4]
 
-    context = {
-        'applications_in_progress_count': applications_in_progress_count,
-        'completed_applications': completed_applications,
-    }
-    return render(request, 'index.html', context)
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'register.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('index')
+
+def login_view(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+    else:
+        form = UserLoginForm()
+    return render(request, 'login.html', {'form': form})
